3d748679f641328e78212f3abab8e721515ae0b7
mingming-ma <mma46@myseneca.ca>
2023-09-15 02:46:59 -0400

    Required Features

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# txt2html
-a command-line tool process input .txt files into generated .html files
+<h1 align="center">text2html</h1>
+
+<div align="center">
+ 
+ [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+</div>
+
+## Features
+
+ - This is a command-line tool process input txt files output html files.
+
+
+## Usage
+Make the tool executable
+```bash
+chmod +x text2html.py
+```
+
+To generate html from a txt file: 
+```bash
+./text2html.py input_file.txt
+```
+To generate html from a folder which has txt files: 
+```bash
+./text2html.py folder-name
+```
+
---
+++ txt2html.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import sys
+import os
+import shutil
+
+
+if len(sys.argv) < 2 or "-h" in sys.argv or "--help" in sys.argv:
+    print("Usage:")
+    print("   To generate html from a txt file: ./text2html.py input_file.txt")
+    print("   To generate html from a folder which has txt files: ./text2html.py folder-name")
+    sys.exit(0)
+elif "-v" in sys.argv or "--version" in sys.argv:
+    print("Version: 0.1")
+    sys.exit(0)
+
+# Create a folder named 'txt2html' or remove it if it exists
+output_folder = 'txt2html'
+if os.path.exists(output_folder):
+    shutil.rmtree(output_folder)
+
+os.makedirs(output_folder)
+
+def process_text_file(input_file):
+    # Read the input text file
+    filename = os.path.splitext(os.path.basename(input_file))[0]
+
+    try:
+        with open(input_file, "r") as file:
+            text_lines = file.readlines()
+    except FileNotFoundError:
+        print(f"Error: {input_file} not found.")
+        exit()
+
+    bodyParagraph = ""
+    for l in text_lines:
+        updatedLine = l.strip()
+        if(len(updatedLine)>1):
+            bodyParagraph += "<p>" + updatedLine + "</p>\n"
+
+
+    # Generate the HTML content
+    html_content = f"""
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>{filename}</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<!-- Generated content here... -->
+{bodyParagraph}
+</body>
+</html>
+"""
+
+    # Write the HTML content to an output file
+    output_file = os.path.join(output_folder, f"{filename}.html")
+
+    try:
+        with open(output_file, "w") as file:
+            file.write(html_content)
+        print(f"HTML file '{output_file}' generated successfully.")
+    except Exception as e:
+        print(f"Error writing to {output_file}: {e}")
+
+def process_folder(input_folder):
+    txt_files = [f for f in os.listdir(input_folder) if f.endswith(".txt")]
+
+    if not txt_files:
+        print(f"No .txt files found in {input_folder}.")
+        return
+    
+    for txt_file in txt_files:
+        # Get the full path to the input .txt file
+        input_file = os.path.join(input_folder, txt_file)
+        process_text_file(input_file)
+
+input_path = sys.argv[1]
+
+if os.path.isfile(input_path):
+    process_text_file(input_path)
+elif os.path.isdir(input_path):
+    process_folder(input_path)
+else:
+    print("Error: Invalid input. Please provide a valid text file or folder.")
+
